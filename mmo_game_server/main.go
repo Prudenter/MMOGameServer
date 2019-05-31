292cb364fab8b455e58260ce4b83949e41ded076
Prudenter <18706733725@163.com>
2019-05-31 22:01:09 +0800

    玩家上线业务Protobuf协议定义,客户端服务器链接测试

--- mmo_game_server/main.go
+++ mmo_game_server/main.go
@@ -6,14 +6,22 @@ package main
 
 import (
 	"ZinxServerFramework/zinx/zinxNet"
+	"ZinxServerFramework/zinx/zinxInterface"
+	"fmt"
 )
 
+//当前客户端建立链接之后触发Hook函数
+func OnConnectionAdd(conn zinxInterface.InterfaceConnection) {
+	fmt.Println("conn Add..")
+
+}
 
 func main() {
 	//创建一个zinx server对象
 	server := zinxNet.NewServer()
 
 	//注册一些创建链接之后或者销毁链接之前的Hook钩子函数
+	server.AddOnConnStart(OnConnectionAdd)
 
 	//启动服务
 	server.Run()
